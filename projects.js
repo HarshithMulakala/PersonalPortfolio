240be5754f4e959b9c00db02519ebce2014b3503
Harshith Mulakala <harshith.mulaka1@gmail.com>
2025-09-05 15:44:55 -0500

    fixed site

--- projects.js
+++ projects.js
@@ -1,40 +1,40 @@
-const swiper = new Swiper(".swiper", {
-    slidesPerView: 6,
-    spaceBetween: 300,
-    loop: true,
-    grabCursor: true,
-    centeredSlides: true,
-    slideActiveClass: "active",
-    navigation: {
-        nextEl: ".next",
-        prevEl: ".prev"
-    },
-    pagination: {
-        el: ".pagination",
-        clickable: true
-    },
-    autoplay: {
-        enabled: true,
-        delay: 5000
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 75
+document.querySelectorAll('.swiper').forEach(function (sliderEl) {
+    new Swiper(sliderEl, {
+        slidesPerView: 6,
+        spaceBetween: 300,
+        loop: true,
+        grabCursor: true,
+        centeredSlides: true,
+        slideActiveClass: "active",
+        navigation: {
+            nextEl: sliderEl.querySelector('.next'),
+            prevEl: sliderEl.querySelector('.prev')
         },
-        645: {
-            slidesPerView: 2,
-            spaceBetween: 150
+        pagination: {
+            el: sliderEl.querySelector('.pagination'),
+            clickable: true
         },
-        // when window width is >= 320px
-        1050: {
-            slidesPerView: 3,
-            spaceBetween: 150
+        autoplay: {
+            enabled: true,
+            delay: 5000
         },
-        // when window width is >= 480px
-        1300: {
-            slidesPerView: 4.1,
-            spaceBetween: 90
-        },
-    }
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 75
+            },
+            645: {
+                slidesPerView: 2,
+                spaceBetween: 150
+            },
+            1050: {
+                slidesPerView: 3,
+                spaceBetween: 150
+            },
+            1300: {
+                slidesPerView: 4.1,
+                spaceBetween: 90
+            },
+        }
+    });
 });
